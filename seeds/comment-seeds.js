b25a44ebe9f740f1d029dd82ae4c071a62b9b9ca
Marie Yee <marieyee02@yahoo.com>
2022-07-26 12:16:28 -0700

    edited comment seeds

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -1,17 +1,17 @@
 const { Comment } = require('../models');
 
 const commentData = [{
-        comment_text: "Lorem ipsum dolor sit amet",
+        comment_text: "This is my first blog.",
         user_id: 1,
         post_id: 1
     },
     {
-        comment_text: "consectetur adipiscing elit",
+        comment_text: "Do you speak lorem ipsum?",
         user_id: 2,
         post_id: 2
     },
     {
-        comment_text: "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        comment_text: "Please Heroku?",
         user_id: 3,
         post_id: 3
     }
